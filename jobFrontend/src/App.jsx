ab531d5743e1561b82f2687a657634249f32b1fa
kingsantus <nkingsantus@gmail.com>
2024-10-04 15:45:07 +0100

    navbar

--- jobFrontend/src/App.jsx
+++ jobFrontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavBar />
       <Outlet />
+      <div>Footer</div>
     </>
   )
 }
